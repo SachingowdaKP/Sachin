d377860260f004b34e40de372bbc033975216b52
SachingowdaKP <sachugkp2002@gmail.com>
2024-11-08 18:12:05 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,17 @@
+package com.day1;
+
+import java.util.Scanner;
+
+public class Exception {
+
+	public static void main(String[] args) {
+		Scanner a=new Scanner (System.in);
+		System.out.println("Enter the value of a:");
+		int s=a.nextInt();
+		System.out.println("Enter the value of b:");
+		int r=a.nextInt();
+		System.out.println(s/r);
+
+	}
+
+}
