36dbe6ed52c0e21b6ccb27241676e9fdc0e9a377
SachingowdaKP <sachugkp2002@gmail.com>
2024-11-08 18:48:52 +0530

    Add files via upload

---
+++ Exception2.java
@@ -0,0 +1,21 @@
+package com.day1;
+
+import java.util.Scanner;
+
+public class Exception2 {
+
+	public static void main(String[] args) {
+		Scanner a=new Scanner (System.in);
+		System.out.println("Enter the value of a:");
+		int s=a.nextInt();
+		System.out.println("Enter the value of b:");
+		int r=a.nextInt();
+		try {
+		System.out.println(s/r);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Dabba fellow, do not divided by 0");
+		}
+
+	}
+}
